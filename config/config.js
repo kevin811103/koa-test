2df3c6a4ec72fe3b68d47b55007b9050798b4b38
eric.chang <eric@wethink.com.tw>
2018-12-28 17:10:18 +0800

    'update'

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const config = {
+    port: 3000,
+    database:{
+
+        DATABASE: 'heroku_6bac947f5fd61be',    //数据库
+        USERNAME: 'b0d95e11b8dc2b',    //用户
+        PASSWORD: '47b2d568',     //密码
+        PORT: '3306',        //端口
+        HOST: 'us-cdbr-iron-east-01.cleardb.net'     //服务ip地址
+    }
+}
+
+module.exports = config
